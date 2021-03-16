cd0719e9a04a3ab36bf8f94c36e4d2e2c8746a91
Alan Sosa <alansosame@MacBook-Pro-de-Alan.local>
2021-03-15 19:41:23 -0600

    Adding swagger documentation and enabling it even JWT is implemented

--- src/main/java/net/alansosa/backendcursojava/security/WebSecurity.java
+++ src/main/java/net/alansosa/backendcursojava/security/WebSecurity.java
@@ -21,6 +21,23 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(org.springframework.security.config.annotation.web.builders.WebSecurity web) throws Exception {
+        //http://localhost:8080/swagger-ui/
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+                // -- Swagger UI v3 (OpenAPI)
+                "/v3/api-docs/**",
+                "/swagger-ui/**");
+                // other public endpoints of your API may be appended to this array);
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         /*
         Here we are configuring the security. By setting the http security parameters
